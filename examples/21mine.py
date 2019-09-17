c73e574ae920c259e814f6237c51df62ac4c8150
kostathai <Tsybikov.KN@phystech.edu>
2019-09-17 15:22:11 +0300

    Initial commit

---
+++ examples/21mine.py
@@ -0,0 +1,18 @@
+from graph import *
+
+canvasSize(500, 800)
+penColor('black')
+x1 = 0
+y1=350
+x2=500
+y2=350
+x3=0
+y3=350
+x4=0
+y4=0
+x5=0
+y=800
+x6=500
+y6=800
+polygon([(x1,y1),(x2,y2), (x3,y3) (x4,y4), (x5,y5)])
+run()
