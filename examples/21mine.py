cbeea8b573de6809d4283dbe8207b347dd771a92
kostathai <Tsybikov.KN@phystech.edu>
2019-09-17 21:11:06 +0300

    Почти сделал дом, но не могу разобраться с овалами. Tkinter не подключается

--- examples/21mine.py
+++ examples/21mine.py
@@ -1,18 +1,58 @@
 from graph import *
 
+
+
+
+
+def cloud(x0, y0, clr1, clr2, clr3):
+
+    b = 8
+
+def rectangle_row(x0, y0, x1, y1, N, dx, clr1, clr2, clr3):
+    brushColor(clr1, clr2, clr3)
+    Length = x1 - x0
+    for i in range(0, N):
+        rectangle(x0, y0, x0 + Length, y1)
+        x0 += Length + dx
+
+
+def backboard():
+    x1 = 0
+    y1 = 250
+    x2 = 500
+    y2 = 800
+
+    x3 = 0
+    y3 = 0
+    x4 = 500
+    y4 = 500
+
+    brushColor(0, 0, 0)
+    rectangle(x1, y1, x2, y2)
+    brushColor(100, 100, 100)
+    rectangle(x3, y3, x4, y4)
+
+
+def house():
+    x1 = 30
+    y1 = 300
+    x2 = 300
+    y2 = 650
+    brushColor(200, 200, 200)
+    rectangle(x1, y1, x2, y2)
+    rectangle_row(x1 + 20, y2 - 175, x1 + 80, y2 - 50, 3, 25, 139, 69, 19)  # окна снизу
+    rectangle_row(x1+20, y1+110, x1+60, y1+40, 5, 10, 34, 213, 33)  # окна сверху
+    rectangle_row(x1, y1 + 70, x1 + 10, y1 + 115, 9, 20, 0, 0, 0)  # перила
+    rectangle(x1 - 10, y1 + 115, x2 - 10, y1 + 120)  # подоконник
+    rectangle(x1 - 10, y1 + 60, x2 - 3, y1 + 80)  # штука на перилах
+    polygon([(x1-20, y1), (x1, y1-20),  (x2, y1-20), (x2+20, y1)])  # крыша
+    penSize(4)
+    line(x2 - 10, y1 + 120, 300, y1 + 110)  # бок пола подокнника
+
+
 canvasSize(500, 800)
 penColor('black')
-x1 = 0
-y1=350
-x2=500
-y2=350
-x3=0
-y3=350
-x4=0
-y4=0
-x5=0
-y=800
-x6=500
-y6=800
-polygon([(x1,y1),(x2,y2), (x3,y3) (x4,y4), (x5,y5)])
+backboard()
+house()
+
 run()
